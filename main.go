0dd6be4a28333ac1f9bfd80789c22c9759df3b5c
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-02-23 17:38:39 -0300

    add index template

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
+
+var templat = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+	http.HandleFunc("/", index)
 	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	templat.ExecuteTemplate(w, "Index", nil)
 }
