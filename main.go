a7d386c133e09808bc32c063b92f6b14977816b7
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-02-23 17:47:27 -0300

    create product list and showing on index template

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+type Product struct {
+	Name        string
+	Description string
+	Price       float64
+	Quantity    int
+}
+
 var templat = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -13,5 +20,9 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	templat.ExecuteTemplate(w, "Index", nil)
+	products := []Product{
+		{Name: "Iphone 99", Description: "New Smart Phone", Price: 1200.00, Quantity: 12},
+		{Name: "Notebook intel 99X", Description: "A New intel notebook", Price: 3000.14, Quantity: 5},
+	}
+	templat.ExecuteTemplate(w, "Index", products)
 }
