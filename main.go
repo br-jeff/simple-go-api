ebf59b8af0fb73d0914d2326eb136ebddbf69121
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-02-23 21:26:33 -0300

    remove old code for import

--- main.go
+++ main.go
@@ -1,73 +1,17 @@
 package main
 
 import (
-	"database/sql"
-	"html/template"
 	"net/http"
-	"time"
 
-	_ "github.com/lib/pq"
+	"github.com/br-jeff/simple-go-htmx/controller"
 )
-
-func connectDB() *sql.DB {
-	connection := "user=usernamepg dbname=db password=password123 host=postgresgo sslmode=disable"
-
-	db, err := sql.Open("postgres", connection)
-
-	if err != nil {
-		panic(err.Error())
-	}
-	return db
-}
-
-type Product struct {
-	Id          int
-	Name        string
-	Description string
-	Price       float64
-	Quantity    int
-}
-
-var templat = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8080", nil)
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	db := connectDB()
+func index(w http.ResponseWriter, req *http.Request) {
+	controller.ProductIndex(w, req)
 
-	selectProduct, err := db.Query("select * from products")
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	p := Product{}
-	products := []Product{}
-
-	for selectProduct.Next() {
-		var id, quantity int
-		var name, description string
-		var price float64
-		var createdAt time.Time
-
-		err = selectProduct.Scan(&id, &name, &description, &price, &quantity, &createdAt)
-
-		if err != nil {
-			panic(err.Error())
-		}
-
-		p.Id = id
-		p.Name = name
-		p.Description = description
-		p.Price = price
-		p.Quantity = quantity
-
-		products = append(products, p)
-	}
-
-	templat.ExecuteTemplate(w, "Index", products)
-	defer db.Close() // Close connection
 }
