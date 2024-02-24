d16e406c0189ac97b1e58593dfc2fad94fc1e3fc
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-02-24 13:47:10 -0300

    create route edit and update

--- route/route.go
+++ route/route.go
@@ -11,4 +11,6 @@ func Bootstrap() {
 	http.HandleFunc("/new", controller.ProductNew)
 	http.HandleFunc("/insert", controller.ProductCreate)
 	http.HandleFunc("/delete", controller.ProductDelete)
+	http.HandleFunc("/edit", controller.ProductEdit)
+	http.HandleFunc("/update", controller.ProductUpdate)
 }
